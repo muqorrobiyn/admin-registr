f3ff41b71ed9a9ba36587a1ce3b1d01f76e33e4d
SMS3384 <akkuntsms8@gmail.com>
2024-12-24 11:33:48 +0500

    practis

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,3 +1,12 @@
+# from django.urls import path
+# from .views import *
+#
+# urlpatterns =[
+#     path(' ', home_page, name='home_page'),
+#     path('login_page/', login_page, name='login_page'),
+#     path('logout_page/', logout_page, name='logout_page'),
+#     path('signup/', SignUpViews.as_view(), name='signup')
+# ]
 from django.urls import path
 from .views import *
 
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,38 +1,38 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect,render
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views import generic
 
+
 def login_required_decorator(func):
-    return login_required(func, login_url='login_page')
+    return login_required(func,login_url='login_page')
 
 
 @login_required_decorator
 def logout_page(request):
     logout(request)
-    return redirect("login_page")
-
+    return redirect('login_page')
 
 def login_page(request):
     if request.POST:
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, password=password, username=username)
+        user = authenticate(request,username=username, password=password )
+        # print(user.password)
         if user is not None:
             login(request, user)
-            return redirect("home_page")
+            return redirect ('home_page')
 
-    return render(request, 'login.html')
-
+    return render (request,'login.html')
 
 @login_required_decorator
 def home_page(request):
-    return render(request, 'index.html')
+    return render(request,'index.html')
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login_page")
-    template_name = "signup.html"
+    success_url = reverse_lazy('login_page')
+    template_name = 'signup.html'
